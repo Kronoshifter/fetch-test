82af013c6b76e9b53f10ff2ec049f533abfc0563
Kronos <kronoshifter@gmail.com>
2025-04-17 01:23:10 -0500

    added convenience methods for converting things to Async

--- app/src/main/java/com/kronos/fetch/ui/screen/FetchTestViewModel.kt
+++ app/src/main/java/com/kronos/fetch/ui/screen/FetchTestViewModel.kt
@@ -1,11 +1,12 @@
 package com.kronos.fetch.ui.screen
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kronos.fetch.model.FetchItem
 import com.kronos.fetch.network.ApiService
 import com.kronos.fetch.network.Async
-import com.kronos.fetch.network.mapError
+import com.kronos.fetch.network.toError
 import com.kronos.fetch.network.toSuccess
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,7 +21,8 @@ import kotlinx.coroutines.flow.stateIn
 import kotlin.time.Duration.Companion.seconds
 
 data class FetchTestUiState(
-  val items: List<FetchItem> = emptyList()
+  val items: List<FetchItem> = emptyList(),
+  val userMessage: String? = null
 )
 
 class FetchTestViewModel(
@@ -29,15 +31,18 @@ class FetchTestViewModel(
   private val _uiState = MutableStateFlow(FetchTestUiState())
   private val _itemsAsync = api.fetchItems()
     .distinctUntilChanged()
-    .map { Async.Success(it) }
+    .map { it.toSuccess() }
     .flowOn(Dispatchers.Default)
-    .catch<Async<List<FetchItem>>> { emit(Async.Error(it.message ?: "Error retrieving items")) }
+    .catch { error ->
+      Log.e("FetchTestViewModel", error.message, error)
+      emit(emptyList<FetchItem>().toError(error.message ?: "Error retrieving items"))
+    }
 
   val uiState = combine(_uiState, _itemsAsync) { uiState, itemsAsync ->
     when (itemsAsync) {
       Async.Loading -> Async.Loading
       is Async.Success -> uiState.copy(items = itemsAsync.data).toSuccess()
-      is Async.Error -> itemsAsync.mapError()
+      is Async.Error -> uiState.copy(userMessage = itemsAsync.message).toError(itemsAsync.message)
     }
   }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5.seconds), Async.Loading)
 }
