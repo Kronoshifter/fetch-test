65cea7640a201e5585fc77fc27599571237802d0
Kronos <kronoshifter@gmail.com>
2025-04-16 22:37:54 -0500

    ide configuration

--- app/src/main/java/com/kronos/fetch/MainActivity.kt
+++ app/src/main/java/com/kronos/fetch/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kronos.fetch.ui.screen.FetchTestScreen
 import com.kronos.fetch.ui.theme.FetchTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,10 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       FetchTestTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
+        FetchTestScreen(
+          modifier = Modifier.fillMaxSize(),
+        )
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  FetchTestTheme {
-    Greeting("Android")
   }
 }
